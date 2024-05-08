44e0794906452b9bae72c360239265c22e272e7c
willdxaava <165103281+willdxaava@users.noreply.github.com>
2024-05-08 20:49:58 -0300

    Update produto.java

--- ex07/produto.java
+++ ex07/produto.java
@@ -1,10 +1,10 @@
-class Produto {
+class produto {
     private String nome;
     private int codigo;
     private double preco;
     private int quantidadeEmEstoque;
 
-    public Produto(String nome, int codigo, double preco, int quantidadeEmEstoque) {
+    public produto(String nome, int codigo, double preco, int quantidadeEmEstoque) {
         this.nome = nome;
         this.codigo = codigo;
         this.preco = preco;
